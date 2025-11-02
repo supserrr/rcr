f67257b6f5b9bc396852e83c802ad3aba08e2055
supserrr <s.shima@alustudent.com>
2025-11-02 18:39:53 +0200

    chore: update eslint config to allow underscore-prefixed unused vars

--- frontend/apps/web/eslint.config.js
+++ frontend/apps/web/eslint.config.js
@@ -1,4 +1,19 @@
 import { nextJsConfig } from "@workspace/eslint-config/next-js"
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig
+export default [
+  ...nextJsConfig,
+  {
+    rules: {
+      // Allow variables and arguments that start with underscore
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ],
+    }
+  }
+]
